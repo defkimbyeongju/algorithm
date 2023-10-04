cdc2bd9fe38d48bfb5cc239098b272ad2f4485bb
Kimbyeongju <qudwn2941@naver.com>
2023-10-04 17:09:22 +0900

    1004

--- study/1005/14502.py
+++ study/1005/14502.py
@@ -1,4 +1,30 @@
+from collections import deque
+# 조합
+def
+
+
+# 바이러스 퍼지기
+def virus(arr):
+    dir = [(0,1),(1,0),(0,-1),(-1,0)]
+    queue = deque()
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 2:
+                queue.append((i,j))
+    while queue:
+        now_y, now_x = queue.popleft()
+        for y,x in dir:
+            ny, nx = now_y+y, now_x+x
+            if 0<=ny<N and 0<=nx<M and arr[ny][nx] == 0:
+                arr[ny][nx] = 2
+                queue.append((ny,nx))
+    arr = sum(arr, [])
+    return arr.count(0)
+
 N,M = map(int,input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-arr = sum(arr, [])
-print(arr.count(0))
+lab = [list(map(int, input().split())) for _ in range(N)]
+safe = [] # 0인 구역
+for i in range(N):
+    for j in range(M):
+        if lab[i][j] == 0:
+            safe.append((i,j))
